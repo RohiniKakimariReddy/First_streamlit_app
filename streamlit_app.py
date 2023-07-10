3e1fb36e8413954b59cb56ee5394ff118a0941dd
RohiniKakimariReddy <136615447+RohiniKakimariReddy@users.noreply.github.com>
2023-07-10 10:45:07 +0100

    Update streamlit_app.py
    added beautiful icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blue Berry Oat meal')
-streamlit.text(' Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-range eggs')
+streamlit.text('🥣  Omega 3 & Blue Berry Oat meal')
+streamlit.text('🥗  Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-range eggs')
+streamlit.text('🥑🍞 Avacado Toast')
