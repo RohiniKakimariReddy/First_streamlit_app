d724a1c31dab6775401c80716da20197777e7555
RohiniKakimariReddy <136615447+RohiniKakimariReddy@users.noreply.github.com>
2023-07-10 10:40:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blue Berry Oat meal')
+streamlit.text(' Kale, Spinach and Rocket Smoothie')
+Stramlit.text('Hard-Boiled Free-range eggs')
