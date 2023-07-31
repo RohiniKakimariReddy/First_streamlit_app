bd3aff69442e94f64c319debefb3dd2485ab89f4
RohiniKakimariReddy <136615447+RohiniKakimariReddy@users.noreply.github.com>
2023-07-31 15:02:22 +0100

    Update streamlit_app.py
    added variable for fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,6 @@ import streamlit
 import pandas
 import requests
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi")
-
-# Normalize the API response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# convert the API response to a dataframe
-streamlit.dataframe(fruityvice_normalized)
-
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣  Omega 3 & Blue Berry Oat meal')
@@ -21,3 +13,14 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick your fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
+# Normalize the API response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# convert the API response to a dataframe
+streamlit.dataframe(fruityvice_normalized)
