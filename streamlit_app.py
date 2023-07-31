c22e93683259bfad23df1c5d3f2172ede6628b10
RohiniKakimariReddy <136615447+RohiniKakimariReddy@users.noreply.github.com>
2023-07-31 14:51:39 +0100

    Update streamlit_app.py
    header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 import pandas
 import requests
+
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
